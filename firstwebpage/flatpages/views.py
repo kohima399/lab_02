e015bdd65fd7f3f9df8f60c06eb26e7052f241ee
Илья <ikingagario@gmail.com>
2025-10-08 10:12:50 +0300

    lab02

---
+++ firstwebpage/flatpages/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse 
+from django.shortcuts import render 
+from django import template 
+
+
+def home(request): 
+    return render(request, 'templates/index.html')
+
